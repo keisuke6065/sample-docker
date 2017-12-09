5d2cc18d3de96e13e96368ca36e2116c4c341a37
中村 恵介 <keisuke.nakamura@bizreach.co.jp>
2017-12-09 20:40:01 +0900

    directory change

--- sampleApp/src/main/java/sample/controller/SampleController.java
+++ sampleApp/src/main/java/sample/controller/SampleController.java
@@ -1,8 +1,10 @@
 package sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sample.controller.appservice.SampleAppService;
 
 /**
  * @author gebara 2017/11/25.
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class SampleController {
+
+	@Autowired
+	private SampleAppService sampleAppService;
 	@GetMapping
 	public String sample() {
+		sampleAppService.samplerun();
 		return "sample";
 	}
 }
