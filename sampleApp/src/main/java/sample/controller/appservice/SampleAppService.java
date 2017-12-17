76bc31aff04038208ffe06e0facc2ea3958a3f14
中村 恵介 <keisuke.nakamura@bizreach.co.jp>
2017-12-17 18:55:41 +0900

    add alb
    todo
    iam role
    nat
    security_group
    associate_public_ip_address = true -> false

--- sampleApp/src/main/java/sample/controller/appservice/SampleAppService.java
+++ sampleApp/src/main/java/sample/controller/appservice/SampleAppService.java
@@ -1,13 +1,6 @@
 package sample.controller.appservice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.springframework.stereotype.Service;
-import sample.controller.entity.Candidate;
 
 /**
  * @author gebara 2017/12/02.
@@ -15,23 +8,6 @@ import sample.controller.entity.Candidate;
 @Service
 public class SampleAppService {
 
-	private static List numberLists = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-	private static List stringLists = Arrays.asList("hoge",
-			"fuge",
-			"sample",
-			"gebara"
-	);
-
-	public void samplerun() {
-		List<Candidate> candidates = new ArrayList<>();
-		for (int i = 0; i < 10; i++) {
-			candidates.add(new Candidate(i, "hoge", true));
-		}
-		Stream<Candidate> stream = candidates.stream();
-		List<Long> collect = candidates.stream()
-				.map(Candidate::getId)
-				.sorted(Long::compareTo)
-				.collect(Collectors.toList());
-		System.out.println(collect);
+	public void sampleRun() {
 	}
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,11 +7,15 @@ variable "region" {
 }
 
 variable "availability-zone" {
-  default = "ap-northeast-1a"
+  type = "list"
+  default = [
+    "ap-northeast-1a",
+    "ap-northeast-1c"
+  ]
 }
 
 variable "ami_images" {
-  default = "ami-95903df3"
+  default = "ami-56bd0030"
 }
 
 variable "instance-type" {
@@ -24,21 +28,9 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_ecs_cluster" "sample-cluster" {
-  name = "sample-cluster"
-}
-
-resource "aws_ecs_task_definition" "sample-task" {
-  container_definitions = "${file("task-definition/smple-task.json")}"
-  family = "sample-task"
-}
-
-resource "aws_ecs_service" "sample-service" {
-  name = "sample-service"
-  task_definition = "${aws_ecs_task_definition.sample-task.arn}"
-  cluster = "${aws_ecs_cluster.sample-cluster.id}"
-  desired_count = 1
-}
+// ==========================================================
+// vpc
+// ==========================================================
 
 resource "aws_vpc" "sample-ecs" {
   cidr_block = "10.1.0.0/16"
@@ -50,14 +42,30 @@ resource "aws_vpc" "sample-ecs" {
   }
 }
 
+
+// ==========================================================
+// nat
+// ==========================================================
+
 resource "aws_internet_gateway" "ecs-gw" {
   vpc_id = "${aws_vpc.sample-ecs.id}"
 }
 
-resource "aws_subnet" "ecs-a-subnet" {
+
+// ==========================================================
+// subnet
+// ==========================================================
+
+resource "aws_subnet" "ecs-subnet-a" {
   cidr_block = "10.1.1.0/24"
   vpc_id = "${aws_vpc.sample-ecs.id}"
-  availability_zone = "${var.availability-zone}"
+  availability_zone = "${var.availability-zone[0]}"
+
+}
+resource "aws_subnet" "ecs-subnet-c" {
+  cidr_block = "10.1.2.0/24"
+  vpc_id = "${aws_vpc.sample-ecs.id}"
+  availability_zone = "${var.availability-zone[1]}"
 }
 
 resource "aws_route_table" "ecs-route" {
@@ -69,20 +77,34 @@ resource "aws_route_table" "ecs-route" {
   }
 }
 
-resource "aws_route_table_association" "ecs-route" {
+resource "aws_route_table_association" "a-route" {
   route_table_id = "${aws_route_table.ecs-route.id}"
-  subnet_id = "${aws_subnet.ecs-a-subnet.id}"
+  subnet_id = "${aws_subnet.ecs-subnet-a.id}"
 }
+resource "aws_route_table_association" "c-route" {
+  route_table_id = "${aws_route_table.ecs-route.id}"
+  subnet_id = "${aws_subnet.ecs-subnet-c.id}"
+}
+
+// ==========================================================
+// security group
+// ==========================================================
 
 resource "aws_security_group" "ecs-sevurity-group" {
   name = "ecs-sevurity-group"
-  description = "Allow SSH inbound traffic"
   vpc_id = "${aws_vpc.sample-ecs.id}"
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    self      = true
+  }
   ingress {
     from_port = 80
     protocol = "tcp"
     to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
   ingress {
     from_port = 22
@@ -93,44 +115,97 @@ resource "aws_security_group" "ecs-sevurity-group" {
   }
 
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-//resource "aws_launch_configuration" "ec2" {
-//  image_id = "${var.ami_images}"
-//  instance_type = "${var.instance-type}"
-//  count = 1
-//  associate_public_ip_address = true
-//  key_name = "${var.ssh_key_name}"
-//  name_prefix = "sample-"
-//  user_data = "#!/bin/bash\necho ECS_CLUSTER='${aws_ecs_cluster.sample-cluster.name}' > /etc/ecs/ecs.config"
-//  security_groups = ["${aws_security_group.ecs-sevurity-group.id}"]
-//  vpc_classic_link_security_groups = ["${aws_vpc.sample-ecs.id}"]
-//  iam_instance_profile = "ecs_iam_role"
-//  root_block_device {
-//    delete_on_termination = true
-//  }
-//}
 
-//resource "aws_autoscaling_group" "bar" {
-//  name = "terraform-asg-example"
-//  launch_configuration = "${aws_launch_configuration.ec2.name}"
-//  min_size = 1
-//  max_size = 2
-//  lifecycle {
-//    create_before_destroy = true
-//  }
-//}
+// ==========================================================
+// iam role
+// ==========================================================
 
+//resource "aws_iam_role" "sample-ecs-iam-role" {
+//  name = "sample-ecs-iam-role"
+//  assume_role_policy = "${file("iam-role/ecs-role.json")}"
+//}
 //
-//output "public ip of sample-docker-instance" {
-//  value = "${aws_launch_configuration.ec2.associate_public_ip_address}"
+//resource "aws_iam_instance_profile" "sample-ecs-iam-instance-profile" {
+//  name = "sample-ecs-iam-instance-profile"
+//  role = "${aws_iam_role.sample-ecs-iam-role.name}"
+//}
+//
+//resource "aws_iam_policy" "sample-ecs-iam-policy" {
+//  name = "sample-ecs-iam-role-policy"
+//  policy = "${file("iam-role/ecs-iam-role-policy.json")}"
+//}
+//
+//resource "aws_iam_role_policy_attachment" "sample-attachment" {
+//  policy_arn = "${aws_iam_policy.sample-ecs-iam-policy.arn}"
+//  role = "${aws_iam_role.sample-ecs-iam-role.name}"
 //}
 
-resource "aws_instance" "sample-docker-instance" {
+// ==========================================================
+// ecs
+// ==========================================================
+
+resource "aws_ecs_cluster" "sample-cluster" {
+  name = "sample-cluster"
+}
+
+resource "aws_ecs_task_definition" "sample-task" {
+  container_definitions = "${file("task-definition/smple-task.json")}"
+  network_mode = "bridge"
+  family = "sample-task"
+}
+
+resource "aws_ecs_service" "sample-service" {
+  name = "sample-service"
+  task_definition = "${aws_ecs_task_definition.sample-task.id}"
+  cluster = "${aws_ecs_cluster.sample-cluster.id}"
+//  iam_role = "${aws_iam_role.sample-ecs-iam-role.id}"
+  desired_count = 2
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.sample-docker-tartget-group.id}"
+    container_name = "sample-task-ver1"
+    container_port = 8080
+  }
+}
+
+// ==========================================================
+// alb
+// ==========================================================
+
+resource "aws_alb_target_group" "sample-docker-tartget-group" {
+  name = "sample-docker-tartget-group"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = "${aws_vpc.sample-ecs.id}"
+}
+
+resource "aws_alb" "sample-docker-alb" {
+  name = "sample-docker-alb"
+  subnets = ["${aws_subnet.ecs-subnet-a.id}","${aws_subnet.ecs-subnet-c.id}"]
+  security_groups = ["${aws_security_group.ecs-sevurity-group.id}"]
+}
+
+resource "aws_alb_listener" "sample-docker-alb-listener" {
+  "default_action" {
+    target_group_arn = "${aws_alb_target_group.sample-docker-tartget-group.id}"
+    type = "forward"
+  }
+  load_balancer_arn = "${aws_alb.sample-docker-alb.id}"
+  port = 80
+  protocol = "HTTP"
+}
+
+// ==========================================================
+// instance
+// ==========================================================
+
+resource "aws_instance" "sample-docker-instance-a" {
   count = 1
   ami = "${var.ami_images}"
   instance_type = "${var.instance-type}"
@@ -140,13 +215,41 @@ resource "aws_instance" "sample-docker-instance" {
     "${aws_security_group.ecs-sevurity-group.id}",
   ]
   iam_instance_profile = "ecsInstanceRole"
-  subnet_id = "${aws_subnet.ecs-a-subnet.id}"
+  subnet_id = "${aws_subnet.ecs-subnet-a.id}"
   tags {
-    Name = "sample-ec2"
+    Name = "sample-ec2-a"
   }
   user_data = "#!/bin/bash\necho ECS_CLUSTER='${aws_ecs_cluster.sample-cluster.name}' > /etc/ecs/ecs.config"
 }
 
-output "public ip of sample-docker-instance" {
-  value = "${aws_instance.sample-docker-instance.public_ip}"
+resource "aws_instance" "sample-docker-instance-c" {
+  count = 1
+  ami = "${var.ami_images}"
+  instance_type = "${var.instance-type}"
+  key_name = "${var.ssh_key_name}"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = [
+    "${aws_security_group.ecs-sevurity-group.id}",
+  ]
+  iam_instance_profile = "ecsInstanceRole"
+  subnet_id = "${aws_subnet.ecs-subnet-c.id}"
+  tags {
+    Name = "sample-ec2-c"
+  }
+  user_data = "#!/bin/bash\necho ECS_CLUSTER='${aws_ecs_cluster.sample-cluster.name}' > /etc/ecs/ecs.config"
 }
+
+
+
+// ==========================================================
+// output
+// ==========================================================
+
+output "public ip of sample-docker-instance-a" {
+  value = [
+    "${aws_instance.sample-docker-instance-a.private_ip}",
+    "${aws_instance.sample-docker-instance-c.private_ip}",
+    "${aws_alb.sample-docker-alb.dns_name}"
+  ]
+}
+
